fc749f9c07a945fe216b6a4eec22fa5706e7d49d
Nhims <quoccuongtran848@gmail.com>
2026-01-20 10:34:16 +0700

    lab3

--- bai3.c
+++ bai3.c
@@ -11,19 +11,52 @@
 #include <stdio.h>
 
 int main() {
-    int soDien;
-    long tienDien;
+    int kWh;
+    long tien = 0;
 
-    // Nhập số điện tiêu thụ
-    printf("Nhap so dien tieu thu trong thang (kWh): ");
-    scanf("%d", &soDien);
+    printf("Nhap so dien tieu thu (kWh): ");
+    scanf("%d", &kWh);
 
-    if (soDien < 0) {
+    if (kWh < 0) {
         printf("So dien khong hop le!\n");
     } else {
-        tienDien = soDien * 3000;
-        printf("So tien dien phai dong la: %ld VND\n", tienDien);
+        if (kWh <= 50) {
+            tien = kWh * 1678;
+        }
+        else if (kWh <= 100) {
+            tien = 50 * 1678
+                 + (kWh - 50) * 1734;
+        }
+        else if (kWh <= 200) {
+            tien = 50 * 1678
+                 + 50 * 1734
+                 + (kWh - 100) * 2014;
+        }
+        else if (kWh <= 300) {
+            tien = 50 * 1678
+                 + 50 * 1734
+                 + 100 * 2014
+                 + (kWh - 200) * 2536;
+        }
+        else if (kWh <= 400) {
+            tien = 50 * 1678
+                 + 50 * 1734
+                 + 100 * 2014
+                 + 100 * 2536
+                 + (kWh - 300) * 2834;
+        }
+        else {
+            tien = 50 * 1678
+                 + 50 * 1734
+                 + 100 * 2014
+                 + 100 * 2536
+                 + 100 * 2834
+                 + (kWh - 400) * 2927;
+        }
+
+        printf("So tien dien phai dong: %ld VND\n", tien);
     }
 
     return 0;
 }
+
